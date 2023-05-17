66472e83a0e0239c4be9cd812885f5e7e6045f95
bacwyls <bacwyls@gmail.com>
2023-05-17 13:43:22 -0500

    frontend: patch z-index bug

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -21,15 +21,15 @@ export function App() {
       return;
     }
     urb.subscribe({
-          app: "houston",
-          path: "/moons",
-          event: handleHoustonSub,
-          quit: subFail,
-          err: subFail
-      })
+      app: "houston",
+      path: "/moons",
+      event: handleHoustonSub,
+      quit: subFail,
+      err: subFail
+    })
       .then((subscriptionId) => {
         setHoustonSub(subscriptionId);
-        });
+      });
   }, [urb, houstonSub]);
 
   // unsub on window close or refresh
@@ -46,14 +46,14 @@ export function App() {
   };
 
 
-  function handleHoustonSub(update:any) {
-        console.log("houston update")
-        setMoons(update["moons"])
-      };
+  function handleHoustonSub(update: any) {
+    console.log("houston update")
+    setMoons(update["moons"])
+  };
 
   function subFail() {
-        console.log("fail!")
-      };
+    console.log("fail!")
+  };
 
   function makeMoon() {
 
@@ -62,13 +62,13 @@ export function App() {
       makeSpecificMoon()
     }
     else {
-    urb.poke({
+      urb.poke({
         app: 'houston',
         mark: 'houston-action',
-        json: {'make-moon':null}
+        json: { 'make-moon': null }
       });
     }
-    };
+  };
 
   function sanitizeMoonInput() {
     const patp = document.getElementById('patp-input')! as HTMLInputElement;
@@ -79,7 +79,7 @@ export function App() {
     } else if (urbitOb.clan(patp.value) != "moon") {
       alert("not a moon");
       return false;
-    } else if (urbitOb.sein(patp.value) != "~"+window.ship) {
+    } else if (urbitOb.sein(patp.value) != "~" + window.ship) {
       alert("not your moon");
       return false;
     } else {
@@ -92,17 +92,17 @@ export function App() {
 
     const patp = document.getElementById('patp-input')! as HTMLInputElement;
 
-    if(!window.confirm(`Are you sure you want to create ${patp.value}? If this moon already exists, its keys will be overwritten!`)){
-        return;
+    if (!window.confirm(`Are you sure you want to create ${patp.value}? If this moon already exists, its keys will be overwritten!`)) {
+      return;
     }
 
     urb.poke({
-        app: 'houston',
-        mark: 'houston-action',
-        json: {'make-my-moon':patp.value}
-      });
+      app: 'houston',
+      mark: 'houston-action',
+      json: { 'make-my-moon': patp.value }
+    });
     patp.value = "";
-    }
+  }
 
   function importMoon() {
     if (!sanitizeMoonInput()) return;
@@ -110,35 +110,35 @@ export function App() {
     const patp = document.getElementById('patp-input')! as HTMLInputElement;
 
     urb.poke({
-        app: 'houston',
-        mark: 'houston-action',
-        json: {'import-moon':patp.value}
-      });
+      app: 'houston',
+      mark: 'houston-action',
+      json: { 'import-moon': patp.value }
+    });
     patp.value = "";
   };
 
 
   return (
     <main className="flex justify-center h-screen"
-           >
+    >
       <div
-      className="h-full w-full absolute top-0 left-0"
-      style={{
-        backgroundImage: `url(${issMoon})`,
-        backgroundPosition: 'center center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundAttachment:'fixed',
-       }}>
+        className="h-full w-full absolute top-0 left-0"
+        style={{
+          backgroundImage: `url(${issMoon})`,
+          backgroundPosition: 'center center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          backgroundAttachment: 'fixed',
+        }}>
       </div>
       <div className="bg-blue-100 w-full md:w-1/2 lg:w-1/2 xl:w-1/2 \
                        md:my-3 lg:my-3 xl:my-3 px-4 bg-opacity-80 \
-                      flex flex-col"
-           style={{backdropFilter: 'blur(36px)'}}>
-        
-        
+                      flex flex-col z-20"
+        style={{ backdropFilter: 'blur(36px)' }}>
+
+
         <h1 className="text-3xl font-bold my-5 text-center">houston</h1>
-        
+
         <div className="flex flex-row mb-2">
           <div className="flex-1">
             <input id={"patp-input"} type="text"
@@ -150,27 +150,27 @@ export function App() {
             className="flex-end"
           >
             <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold ml-2 py-2 px-4"
-                    onClick={makeMoon}
-              >create</button>
+              onClick={makeMoon}
+            >create</button>
 
             <button className="bg-green-600 hover:bg-green-700 text-white font-bold ml-2 py-2 px-4"
-                    onClick={importMoon}
-              >import</button>
+              onClick={importMoon}
+            >import</button>
           </div>
         </div>
 
         <div
           className="flex-end overflow-y-scroll"
-          // style={{
-          //   height:'70%'
-          // }}
+        // style={{
+        //   height:'70%'
+        // }}
         >
           {moons.map(mon => (
-              <MoonTile urb={urb}
-                        moon={mon}
-                        key={mon["who"]}
-              />
-            ))
+            <MoonTile urb={urb}
+              moon={mon}
+              key={mon["who"]}
+            />
+          ))
           }
         </div>
       </div>
